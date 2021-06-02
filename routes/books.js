e57862b1025bbde7250d31f5af771faba167dda2
zolboo0918 <59473765+zolboo0918@users.noreply.github.com>
2021-06-02 13:53:59 +0800

    deletebook

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { getBooks, getBook } = require("../controller/books");
+const { getBooks, getBook, deleteBook } = require("../controller/books");
 const { writeComment } = require("../controller/comments");
 const { checkToken } = require("../middleware/protect");
+
 
 const router = express.Router();
 
 router.route("/").get(getBooks);
 router.route("/:id").get(getBook);
+router.route("/:id).delete(deleteBook);
 
 router.route("/:id/comments").post(checkToken, writeComment);
 
