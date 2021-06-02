af531d5ce7e69a41bb072ff49eec2058a17abfc9
zolboo918 <zolboo412@gmail.com>
2021-06-02 16:27:57 +0800

    cors

--- server.js
+++ server.js
@@ -30,15 +30,16 @@ var whitelist = ["http://localhost:3000"];
 // Өөр домэйн дээр байрлах клиент вэб аппуудаас шаардах шаардлагуудыг энд тодорхойлно
 var corsOptions = {
   // Ямар ямар домэйнээс манай рест апиг дуудаж болохыг заана
-  origin: function (origin, callback) {
-    if (origin === undefined || whitelist.indexOf(origin) !== -1) {
-      // Энэ домэйнээс манай рест рүү хандахыг зөвшөөрнө
-      callback(null, true);
-    } else {
-      // Энэ домэйнд хандахыг хориглоно.
-      callback(new Error("Horigloj baina.."));
-    }
-  },
+  origin: "*",
+  // function (origin, callback) {
+  //   if (origin === undefined || whitelist.indexOf(origin) !== -1) {
+  //     // Энэ домэйнээс манай рест рүү хандахыг зөвшөөрнө
+  //     callback(null, true);
+  //   } else {
+  //     // Энэ домэйнд хандахыг хориглоно.
+  //     callback(new Error("Horigloj baina.."));
+  //   }
+  // },
   // Клиент талаас эдгээр http header-үүдийг бичиж илгээхийг зөвшөөрнө
   allowedHeaders: "Authorization, Set-Cookie, Content-Type",
   // Клиент талаас эдгээр мэссэжүүдийг илгээхийг зөвөөрнө
@@ -83,9 +84,7 @@ app.use("/api/v1/categories", categoryRoute);
 
 app.use(errorHandler);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`server ${process.env.PORT} амжилттай ажиллаа`)
-);
+const server = app.listen(process.env.PORT, () => console.log(`server ${process.env.PORT} амжилттай ажиллаа`));
 
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Алдаа гарлаа : ${err.message}`);
