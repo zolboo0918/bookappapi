b4f72a814a22a8e27f7fac4125a113df8ced9651
ЗОЛБОО Жаргалсайхан <zolboo.j@interactive.local>
2021-06-02 16:06:11 +0800

    d

--- routes/books.js
+++ routes/books.js
@@ -3,12 +3,11 @@ const { getBooks, getBook, deleteBook } = require("../controller/books");
 const { writeComment } = require("../controller/comments");
 const { checkToken } = require("../middleware/protect");
 
-
 const router = express.Router();
 
 router.route("/").get(getBooks);
 router.route("/:id").get(getBook);
-router.route("/:id).delete(deleteBook);
+router.route("/:id").delete(deleteBook);
 
 router.route("/:id/comments").post(checkToken, writeComment);
 
